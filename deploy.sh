b5a58772f8e03992d984266e2c52efef097d7451
FredFreeb <frederic.fribel@hotmail.fr>
2026-05-01 16:20:00 +0200

    Use schema update for production deploy

--- deploy.sh
+++ deploy.sh
@@ -26,7 +26,7 @@ run docker compose --env-file .env.prod.local -f compose.prod.yaml build --pull
 run docker compose --env-file .env.prod.local -f compose.prod.yaml up -d database php nginx
 run docker compose --env-file .env.prod.local -f compose.prod.yaml exec -T --user root php chown -R www-data:www-data /var/www/html/var
 run docker compose --env-file .env.prod.local -f compose.prod.yaml exec -T php composer install --prefer-dist --no-dev --no-interaction --no-progress --optimize-autoloader
-run docker compose --env-file .env.prod.local -f compose.prod.yaml exec -T php php bin/console doctrine:migrations:migrate --no-interaction --env=prod
+run docker compose --env-file .env.prod.local -f compose.prod.yaml exec -T php php bin/console doctrine:schema:update --force --complete --no-interaction --env=prod
 run docker compose --env-file .env.prod.local -f compose.prod.yaml exec -T php php bin/console cache:clear --env=prod --no-debug
 run docker compose --env-file .env.prod.local -f compose.prod.yaml exec -T php php bin/console cache:warmup --env=prod --no-debug
 run docker compose --env-file .env.prod.local -f compose.prod.yaml exec -T --user root php chown -R www-data:www-data /var/www/html/var /var/www/html/public/uploads
